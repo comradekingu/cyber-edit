3a00a49bffe8fce353098b61eece237f35697fb2
Aren Coşkun <ar3n1596@gmail.com>
2021-05-20 21:01:55 +0200

    Added translation using Weblate (Turkish)

---
+++ translations/tr_TR.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/qml/main.qml" line="14"/>
+        <source>Text Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="51"/>
+        <source>New File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="56"/>
+        <source>Open File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="61"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="66"/>
+        <source>Run...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="71"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="72"/>
+        <source>Cyber Text Editor
+Text Editor for the Cyber desktop environment.
+
+Made by aren with Qt Quick and C++.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="72"/>
+        <source>About Text Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="147"/>
+        <location filename="../src/qml/main.qml" line="181"/>
+        <location filename="../src/qml/main.qml" line="213"/>
+        <location filename="../src/qml/main.qml" line="243"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="156"/>
+        <source>GitHub Repository</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="164"/>
+        <location filename="../src/qml/main.qml" line="176"/>
+        <source>Enter file name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="195"/>
+        <source>Are you sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="207"/>
+        <source>Are you sure? You will lose unsaved changes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="226"/>
+        <location filename="../src/qml/main.qml" line="238"/>
+        <source>Enter command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="255"/>
+        <source>Please choose a file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="271"/>
+        <source>Please choose where to save the file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
