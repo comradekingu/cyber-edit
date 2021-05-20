eedd67a98ad949d043d523aa8a16dc1e960fc85a
Aren <64262547+arencos@users.noreply.github.com>
2021-05-20 21:16:19 +0300

    fix link

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 if [ ! -d "build" ] 
 then
-    echo "You need to build before you can install. Check https://github.com/arencos/cyber-edit for instructions." 
+    echo "You need to build before you can install. Check https://github.com/cyberos/cyber-edit for instructions." 
     exit 1
 fi
 sudo rm /usr/share/applications/cyber-edit.desktop
