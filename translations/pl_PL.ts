257fe03d9c05af8f63bf505566a6ad45394b4a71
omame <me@omame.tech>
2021-05-20 18:49:22 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: CyberOS/Text Editor
    Translate-URL: https://hosted.weblate.org/projects/cyberos/text-editor/pl/

--- translations/pl_PL.ts
+++ translations/pl_PL.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../src/qml/main.qml" line="14"/>
         <source>Text Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>Edytor tekstu</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="51"/>
         <source>New File</source>
-        <translation type="unfinished"></translation>
+        <translation>Nowy plik</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="56"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Otwórz plik</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="61"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisz</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="66"/>
         <source>Run...</source>
-        <translation type="unfinished"></translation>
+        <translation>Uruchom...</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="71"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>O programie</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="72"/>
@@ -39,12 +39,15 @@
 Text Editor for the Cyber desktop environment.
 
 Made by aren with Qt Quick and C++.</source>
-        <translation type="unfinished"></translation>
+        <translation>Edytor tekstu
+Edytor tekstu zrobiony dla środowiska graficznego Cyber.
+
+Zrobiony przez aren w Qt Quick i C++.</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="72"/>
         <source>About Text Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>O edytorze tekstu</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="147"/>
@@ -52,44 +55,44 @@ Made by aren with Qt Quick and C++.</source>
         <location filename="../src/qml/main.qml" line="213"/>
         <location filename="../src/qml/main.qml" line="243"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="156"/>
         <source>GitHub Repository</source>
-        <translation type="unfinished"></translation>
+        <translation>Repozytorium GitHub</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="164"/>
         <location filename="../src/qml/main.qml" line="176"/>
         <source>Enter file name</source>
-        <translation type="unfinished"></translation>
+        <translation>Wpisz nazwę pliku</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="195"/>
         <source>Are you sure?</source>
-        <translation type="unfinished"></translation>
+        <translation>Czy jesteś pewien?</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="207"/>
         <source>Are you sure? You will lose unsaved changes.</source>
-        <translation type="unfinished"></translation>
+        <translation>Czy jesteś&#xa0;pewien? Utracisz niezapisane zmiany.</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="226"/>
         <location filename="../src/qml/main.qml" line="238"/>
         <source>Enter command</source>
-        <translation type="unfinished"></translation>
+        <translation>Wpisz komendę</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="255"/>
         <source>Please choose a file</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz plik</translation>
     </message>
     <message>
         <location filename="../src/qml/main.qml" line="271"/>
         <source>Please choose where to save the file</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz lokalizację zapisanego pliku</translation>
     </message>
 </context>
 </TS>
